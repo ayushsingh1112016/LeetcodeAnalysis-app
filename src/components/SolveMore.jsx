2e63340c9d741d83c8dc09903e302dd12da5af7c
ayushsingh1112016 <91727730+ayushsingh1112016@users.noreply.github.com>
2024-07-25 10:53:25 +0530

    Update SolveMore.jsx

--- src/components/SolveMore.jsx
+++ src/components/SolveMore.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const SolveMore = ({username}) => {
     return(
         <div className="flex w-full ">
-            <Link to={`/${username}/problems`} className="w-full text-center py-4 px-4 cursor-pointer rounded-xl text-white bg-black text-xl"> Solve According to Rating </Link>
+            <Link to={`/${username}/problems`} className="w-full text-center py-4 px-4 cursor-pointer rounded-xl text-white bg-black text-xl"> Click To Solve More </Link>
         </div>
     );
 }
